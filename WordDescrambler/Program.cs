2484169f5d48041e5de0cc4605f9d2032d7842c3
mmaluschnig <mitchie.maluschnig@gmail.com>
2024-04-18 08:12:29 +1200

    added input validation

--- WordDescrambler/Program.cs
+++ WordDescrambler/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Word Descrambler
+using System.Text.RegularExpressions;
+
+
+// get scrambled word and validate input
+
+String scrambledWord;
+do
+{
+    //TODO: Add try-catch
+    Console.Write("Please enter a scrambled word:");
+    scrambledWord = Console.ReadLine();
+
+} while (!isStringOnlyLetters(scrambledWord));
+
+
+Console.WriteLine($"Scrambled word is: {scrambledWord}.");
+
+static bool isStringOnlyLetters(string s)
+{
+    if (string.IsNullOrWhiteSpace(s))
+    {
+        Console.WriteLine("Invalid word. Please try again.");
+        return false;
+    }
+
+    if (!Regex.IsMatch(s, @"^[a-zA-Z]+$"))
+    {
+        Console.WriteLine("Word must contain only upper and lower case letters. Please try again.");
+        return false;
+    };
+
+    return true;
+}
